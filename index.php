6741e7bb020db40c2a89d2431be2154910e50dd6
McRae <McRaeSoftware@gmail.com>
2020-12-29 21:26:33 +0000

    altered some styling of buttons and text. Also added an intor animation.

--- index.php
+++ index.php
@@ -16,11 +16,27 @@
 </head>
 
 <body>
+<div class="row">
 
-  <div class="row">
-    <div class="w50">1</div>
-    <div class="w50">2</div>
+  <div class="w50">
+    <h1 class="text-light">David McRae</h1>
+    <h3>Portfolio</h3>
   </div>
 
+</div>
+<div class="row">
+  <h4>This portfolio is to Showcase some projects i have worked on in the past</h4>
+</div>
+
+
+
+<div class="row">
+  <div class="w50">
+    <button class="btn white">Default</button>
+  </div>
+</div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" integrity="sha256-qM7QTJSlvtPSxVRjVWNM2OfTAz/3k5ovHOKmKXuYMO4=" crossorigin="anonymous"></script>
 </body>
 </html>
