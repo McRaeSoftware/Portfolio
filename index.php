a5b10af6f8df05d0bba57728349eac00ef94fcc8
McRae <McRaeSoftware@gmail.com>
2020-12-29 17:45:46 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- image for tabs in browser -->
+    <link rel="icon" href="Images/McRaeChevron.jpg">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
+
+    <!-- Custom Css -->
+    <style>
+    <?php include 'theme.css'; ?>
+    </style>
+</head>
+
+<body>
+
+  <div class="row">
+    <div class="w50">1</div>
+    <div class="w50">2</div>
+  </div>
+
+</body>
+</html>
